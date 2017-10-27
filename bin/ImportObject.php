7c024f48516aef23ea8d2e8b4d82639ccc1bc4c1
jcjnyc <jjackson@cpxi.com>
2017-10-26 20:01:20 -0400

    initial commit of DFP sync tool

---
+++ bin/ImportObject.php
@@ -0,0 +1,8 @@
+<?php
+
+require(__DIR__.'/../vendor/autoload.php');
+require(__DIR__.'/../lib/GetAllLineItems.php');
+//require(__DIR__.'/../lib/GetAllOrders.php');
+//require(__DIR__.'/../lib/GetAllLicas.php');
+
+
